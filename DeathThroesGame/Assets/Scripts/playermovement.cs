57c14c569db0475bcff2104ca67acd5711eea149
Landon Clarke <89565789+Larke20@users.noreply.github.com>
2024-03-21 19:34:46 -0500

    player animation and movement added

---
+++ DeathThroesGame/Assets/Scripts/playermovement.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Vector2 moveInput;
+    [SerializeField]private float speed;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        //l,r,u,d movement
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+        body.velocity = moveInput * speed;
+
+        //l,r flip
+        if(moveInput.x > 0.01f){
+            transform.localScale = new Vector3(0.18f, 0.20f, 1);
+        }
+        else if(moveInput.x < -0.01f){
+            transform.localScale = new Vector3(-0.18f,0.20f,1);
+        }
+        //animation
+        bool isMoving = moveInput.magnitude > 0.01f;
+        anim.SetBool("run", isMoving);
+        
+    }
+}
