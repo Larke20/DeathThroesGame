f4b33889ca5d46012cb917667d3eea9aef2797b7
mechols1 <mechol8@lsu.edu>
2024-04-11 16:47:33 -0500

    Added attacking animation and Input for attacking
    Still need to add hitboxing

--- DeathThroesGame/Assets/Scripts/playermovement.cs
+++ DeathThroesGame/Assets/Scripts/playermovement.cs
@@ -14,6 +14,17 @@ public class playermovement : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
+
+    private void Attack(){
+
+        anim.SetBool("attack", true);
+
+    }
+    public void FinishAttack()
+    {
+        anim.SetBool("attack", false);
+        anim.SetTrigger("finishAttack");
+    }
     private void Update()
     {
         //l,r,u,d movement
@@ -30,6 +41,12 @@ public class playermovement : MonoBehaviour
         else if(moveInput.x < -0.01f){
             transform.localScale = new Vector3(-0.18f,0.20f,1);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+
         //animation
         bool isMoving = moveInput.magnitude > 0.01f;
         anim.SetBool("run", isMoving);
