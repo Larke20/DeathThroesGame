cabfd0b61c60ef674cf69f33900441930e0c4991
mechols1 <mechol8@lsu.edu>
2024-05-10 00:25:55 -0500

    FIREBALL WORKS FELLAS HIP HIP HOORAY

--- DeathThroesGame/Assets/fireballActive.cs
+++ DeathThroesGame/Assets/fireballActive.cs
@@ -7,19 +7,25 @@ public class fireballActive : MonoBehaviour
     private Rigidbody2D rb;
     public float speed;
     public float timer;
+    private int damage = 50;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;   
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(GetComponent<Collider>() != null)
+        if(collider.GetComponent<Health>() != null)
         {
-                int damage = 50;
-                Health health = GetComponent<Collider>().GetComponent<Health>();
-                health.Damage(damage);
+            Health health = collider.GetComponent<Health>();
+            health.Damage(damage);
+        }
+        if(collider.GetComponent<BossHealth>() != null)
+        {
+            BossHealth bossHealth = collider.GetComponent<BossHealth>();
+            bossHealth.BossTakeDamage(damage);
         }
         timer += Time.deltaTime;
         if(timer > 1)
