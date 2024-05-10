3524ea57da14cd14bf9f72eff5e896fa0fad909c
mechols1 <mechol8@lsu.edu>
2024-05-10 00:20:23 -0500

    fixed fireball

--- DeathThroesGame/Assets/fireballActive.cs
+++ DeathThroesGame/Assets/fireballActive.cs
@@ -6,6 +6,7 @@ public class fireballActive : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +15,17 @@ public class fireballActive : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-    if(GetComponent<Collider>() != null)
-    {
-            int damage = 50;
-            Health health = GetComponent<Collider>().GetComponent<Health>();
-            health.Damage(damage);
-    }
+        if(GetComponent<Collider>() != null)
+        {
+                int damage = 50;
+                Health health = GetComponent<Collider>().GetComponent<Health>();
+                health.Damage(damage);
+        }
+        timer += Time.deltaTime;
+        if(timer > 1)
+        {
+            Destroy(gameObject);
+        }
     }
     
 
